30bb907e0ae915306ac2bd8c20927fc7ae8ed060
KariukiAntony <antonygichoya9@gmail.com>
2026-03-05 10:44:53 +0300

    Update: new admin stats included

--- server/src/controllers/admin.controller.js
+++ server/src/controllers/admin.controller.js
@@ -2,53 +2,12 @@ import { userModel } from "../models/user.model.js";
 import { ApiError } from "../utils/APiError.js";
 import { catchAsync } from "../utils/catchAsync.js";
 import httpStatus from "http-status";
-import moment from "moment";
-import { accountApprovalEmail } from "../utils/formatEmail.js";
-import { allowList, blockList, sendEmail } from "../utils/utils.js";
+import { allowList, blockList } from "../utils/utils.js";
 import adminService from "../services/admin.service.js";
-import paymentModel from "../models/payment.model.js";
 
 const adminStats = catchAsync(async (req, res) => {
-  const pipeline = [
-    {
-      $facet: {
-        totalUsers: [{ $count: "count" }],
-        verifiedUsers: [{ $match: { isVerified: true } }, { $count: "count" }],
-        pendingApprovals: [
-          { $match: { isSpecial: true, isApproved: false } },
-          { $count: "count" },
-        ],
-        approvedUsers: [{ $match: { isApproved: true } }, { $count: "count" }],
-        specialUsers: [{ $match: { isSpecial: true } }, { $count: "count" }],
-        roleDistribution: [{ $group: { _id: "$role", count: { $sum: 1 } } }],
-        recentUsers: [
-          {
-            $match: {
-              joined: { $gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000) },
-            },
-          },
-          { $count: "count" },
-        ],
-        userGrowth: [
-          { $group: { _id: { $month: "$joined" }, count: { $sum: 1 } } },
-          { $sort: { _id: 1 } },
-        ],
-      },
-    },
-  ];
-  const salesPipeline = [
-    { $match: { status: "completed" } },
-    {
-      $group: {
-        _id: { $month: "$createdAt" },
-        totalSales: { $sum: "$amount" },
-      },
-    },
-    { $sort: { _id: 1 } },
-  ];
-  const data = await userModel.aggregate(pipeline);
-  const salesStats = await paymentModel.aggregate(salesPipeline);
-  return res.status(httpStatus.OK).json({ userData: data, salesData: salesStats });
+  const [userData, salesData, orderData] = await adminService.getAdminStats();
+  return res.status(httpStatus.OK).json({ userData, salesData, orderData });
 });
 
 const manageUsers = catchAsync(async (req, res) => {
